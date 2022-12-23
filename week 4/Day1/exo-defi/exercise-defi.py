8a64c4513b09fab1c3b70c65064f2d7015b63a9b
thomas <sawadogothomas107@gmail.com>
2022-12-23 14:18:25 +0000

    PYTHON1

---
+++ week 4/Day1/exo-defi/exercise-defi.py
@@ -0,0 +1,15 @@
+# exercise 1
+chaine = input("entrer une chaîne de caractère")
+if len(chaine) < 10:
+    print("chaîne pas assez long")
+elif len(chaine) > 10:
+    print("chaîne trop long")
+elif len(chaine) == 10:
+    print("taille de la chaine est respectée")
+
+# exercise 2
+print(f" le premier caratère est {chaine[0]}\n, le dernier caractère est {chaine[-1]}\n")
+
+# exercise 3
+chaine1 = input("entrer une chaine de caratère")
+
